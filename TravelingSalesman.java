e7550082ae4c3eb8a3375c5a9db157725b8f8d45
ElizaKnapp <eknapp20@stuy.edu>
2021-01-19 12:38:09 -0500

    cities in an array so now they each have an index

--- TravelingSalesman.java
+++ TravelingSalesman.java
@@ -3,6 +3,19 @@ import java.io.*;
 
 public class TravelingSalesman {
   public static void main(String[] args) {
-    
+
+    //here is the arraylist of cities
+    Scanner scanner = new Scanner (Cities.txt);
+    ArrayList<String> cities = new ArrayList<String>();
+    while (scanner.hasNextLine()) {
+      Scanner line = new Scanner(scanner.NextLine());
+      String city1 = line.next();
+      line.next();
+      String city2 = line.next();
+      if (!cities.contains(city1)) cities.add(city1);
+      if (!cities.contains(city2)) cities.add(city2);
+    }
+
+    //
   }
 }
